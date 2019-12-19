04e979edeec1e149bc0202112cfba98cfcfbbd5f
charles <syusikoku@yahoo.com>
2019-12-19 16:38:15 +0800

    cmakelist脚本 优化

--- SampleFfmpegV421/app/src/main/java/com/zy/ffmpegv421/MainActivity.java
+++ SampleFfmpegV421/app/src/main/java/com/zy/ffmpegv421/MainActivity.java
@@ -12,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+        System.loadLibrary("avdevice-57");
+        System.loadLibrary("postproc-54");
+
         System.loadLibrary("native-lib");
     }
 
